d3d571b24a0612ca040febb473e335bb2b6c1ce8
Jason Luo <jason.cc.luo@mail.foxconn.com>
2017-12-21 13:42:26 +0800

    fix: not to reset wdt clock source when execute wdt2 disable command
    Change-Id: I2faebace6c50975232168b5bb8c7ff064c48a85b
    Signed-off-by: Jason Luo <jason.cc.luo@mail.foxconn.com>

--- cmd/wdt2.c
+++ cmd/wdt2.c
@@ -6,13 +6,22 @@
 
 #include <common.h>
 #include <command.h>
+#include <asm/io.h>
+
+void wdt2_disable()
+{
+    u32 reg;
+    reg = readl(AST_WDT_BASE + 0x2c);
+    reg &= 0xFFFFFFFE;
+    writel(reg, AST_WDT_BASE + 0x2c);
+}
 
 int do_wdt2_disable(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[])
 {
     if (argc != 2)
         return CMD_RET_USAGE;
     if (argv[1][0] == '0')
-        *((volatile ulong *)0x1e78502c) = 0;
+        wdt2_disable();
     else
          return CMD_RET_USAGE;
 
